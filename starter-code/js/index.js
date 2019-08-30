8fd39d41807bf480f70c7860396c467777d8a3da
Stefan Grulke <grulke@mediamarktsaturn.com>
2019-08-30 17:32:56 +0200

    Daria,StefanG Car

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,21 +1,40 @@
+let divWrapper = document.getElementById("wrapper");
+
 function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(product){
+   //itemNode - paramübergabe
+  // const basePrice = Array.from(document.getElementsByClassName('productPrice')).map(price => price.innerHTML.substring(1,price.length));
+ const productPrice = product.getchildNodes[0].innerHTML ;
+ const productQuantity = product.getchildNodes.getElementById("input").innerHTML ;
 
+ console.log("erhzearhzaerhzae", productPrice, productQuantity )
+  const price = parseFloat(productPrice)*parseFloat(productQuantity);
+  return price;
 }
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
+function getTotalPrice(event) {
+  const product = document.getElementById("input").innerHTML;
 
+  console.log(product);
+  
+  
+  let sum = 0;
+
+
+  // totalBox.innerHTML="$" + sum;
+  return sum;
 }
 
 function createQuantityInput(){
-
+  let quantity = document.getElementsByClassName("qtyInput").innerHTML;
+  console.log(quantity);
 }
 
 function createDeleteButton(){
@@ -39,14 +58,22 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  //var calculatePriceButton = document.getElementById('btn-success');
+  //var createItemButton = document.getElementById('new-item-create');
+  //var deleteButtons = document.getElementsByClassName('btn-delete');
+  document.getElementById('btn-success').onclick=getTotalPrice;
+  /*let quantity = document.getElementById("input").innerHTML;
+  const productAmount = Array.from(document.getElementsByClassName('productPrice')).map(x => x.innerHTML.substring(1,x.length));
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  console.log(productAmount);
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+ 
+  console.log(quantity);  */
+  //calculatePriceButton.onclick = getTotalPrice();
+  //createItemButton.onclick = createNewItem;
+
+ /* for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
-  }
+  }*/
 };
+
